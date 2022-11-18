a29550a30a4032611160f772cfbe95ef40ed23b2
Daniel Ramirez <djram2898@outlook.com>
2022-11-18 12:05:36 -0600

    Adding forecasting features for next day

--- main.py
+++ main.py
@@ -74,3 +74,66 @@ def search():
             degree=wind["degree"],direction=wind["dir"],
             kmh=wind["speed_kph"],mph=wind["speed_mph"])
 
+@app.route("/forecast",methods=["POST"])
+def forecast():
+    # URLs for HTTP POST requests
+    url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
+
+    # Headers info { API key & and API host } to make API calls
+    headers = {
+        "X-RapidAPI-Key": "153f94ba4amsh2b77e486cbac9e4p18bfaejsn63e24918a3ba",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+    }   
+
+    # Take user input and save it as the variable 'city'
+    city = form.form["city"]
+
+    # Exception Handling: Empty Input ==> Refresh Main Page
+    if city == "":
+        return render_template("index.html",error_message="Please enter a city")
+    
+    # Otherwise, take the entered user input 'city' to the query
+    querystring = {"q":city,"days":"3"}
+
+    # First API call for the real-time weather info in the city
+    forecast_response = requests.request("GET", url, headers=headers, params=querystring)
+    forecast_data = json.loads(forecast_response.text)
+
+    # Exception Handling: invalid user input
+    # When the user gives an unvalid input other than empty string
+    # return 4xx Error 
+    if (forecast_response.status_code in range(400,500)):
+        return render_template("index.html",error_message="No Matching Found Location")
+    
+    # Exception Handling: buggy codes/internal server issues
+    # When some codes are not working properly
+    # return 5xx Error
+    if (forecast_response.status_code in range(500,600)):
+        return render_template("error.html",status_code=forecast_response.status_code,error_message=forecast_response.reason)
+    
+    # Otherwise (i.e. it is a valid input which is a city/town), return the info
+    # if forecast_data["location"]["region"] == "" or forecast_data["location"]["region"] == forecast_data["location"]["name"] :
+    #     city = forecast_data["location"]["name"]+", "+forecast_data["location"]["country"]
+    # else:
+    #     city = forecast_data["location"]["name"]+", "+forecast_data["location"]["region"]
+    # time = forecast_data["location"]["localtime"]
+    # sunrise = forecast_data["astronomy"]["astro"]["sunrise"]
+    # sunset = forecast_data["astronomy"]["astro"]["sunset"]
+
+    # condition = forecast_data["current"]["condition"]["text"] # e.g. cloudy, sunny, foggy, etc.
+    # temp = {"C":forecast_data["current"]["temp_c"],"F":forecast_data["current"]["temp_f"],
+    #                 "Real-Feel C":forecast_data["current"]["feelslike_c"],"Real-Feel F":forecast_data["current"]["feelslike_f"]}
+    # wind = {"degree":forecast_data["current"]["wind_degree"],"dir":forecast_data["current"]["wind_dir"],
+    #         "speed_kph":forecast_data["current"]["wind_kph"],"speed_mph":forecast_data["current"]["wind_mph"]}
+
+    # return render_template("forecast.html",location=city,
+    #         time=time,sunrise=sunrise,sunset=sunset,
+    #         desc=condition,celsius=temp["C"],fahrenheit=temp["F"],
+    #         realfeel_c=temp["Real-Feel C"],realfeel_f=temp["Real-Feel F"],
+    #         degree=wind["degree"],direction=wind["dir"],
+    #         kmh=wind["speed_kph"],mph=wind["speed_mph"])
+    tomorrow = str(forecast_data["forecast"]["forecastday"][1]["date"])
+    tomorrow_data = forecast_data["forecast"]["forecastday"][1]["day"]
+    tomorrow_astro = forecast_data["forecast"]["forecastday"][1]["astro"]
+
+    return tomorrow+"\n"+str(tomorrow_data["maxtemp_f"])+"\n"+str(tomorrow_data["mintemp_f"])+"\n"+str(tomorrow_astro["sunrise"])
