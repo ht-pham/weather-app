2d85f7deb176330b38fdad91222a748bbaf39065
Huong Pham <96hpham@gmail.com>
2022-11-22 18:31:22 -0600

    Security fix: API key and API host have been hidden.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import request as form
 
 import requests
 import json
+import config
 
 app = Flask(__name__,template_folder="templates")
 
@@ -24,28 +25,23 @@ def search():
     if(form.form["action"]=="now"):
 
         # URLs for HTTP POST requests
-        url_current = "https://weatherapi-com.p.rapidapi.com/current.json"
-        url_astronomy = "https://weatherapi-com.p.rapidapi.com/astronomy.json"
-
-        # Headers info { API key & and API host } to make API calls
-        headers = {
-            "X-RapidAPI-Key": "153f94ba4amsh2b77e486cbac9e4p18bfaejsn63e24918a3ba",
-            "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
-        }   
+        url_current = config.base_url+"current.json"
+        url_astronomy = config.base_url+"astronomy.json"
+  
         # Take the entered user input 'city' to the query
         querystring = {"q":city}
 
         # First API call for the real-time weather info in the city
-        response_current = requests.request("GET", url_current, headers=headers, params=querystring)
+        response_current = requests.request("GET",url_current, headers=config.headers, params=querystring)
         current_data = json.loads(response_current.text)
         # Second API call for the astronomy info of the city (i.e. sunrise and sunset time)
-        response_astronomy = requests.request("GET", url_astronomy, headers=headers, params=querystring)
+        response_astronomy = requests.request("GET", url_astronomy, headers=config.headers, params=querystring)
         astro_data = json.loads(response_astronomy.text)
 
         # Exception Handling: invalid user input
         # When the user gives an unvalid input other than empty string
         # return 4xx Error 
-        if (response_current.status_code in range(400,500)):
+        if (response_current.status_code in range(400,501)):
             return render_template("index.html",error_message="No Matching Found Location")
         
         # Exception Handling: buggy codes/internal server issues
@@ -59,6 +55,7 @@ def search():
             city = current_data["location"]["name"]+", "+current_data["location"]["country"]
         else:
             city = current_data["location"]["name"]+", "+current_data["location"]["region"]
+        
         time = current_data["location"]["localtime"]
         sunrise = astro_data["astronomy"]["astro"]["sunrise"]
         sunset = astro_data["astronomy"]["astro"]["sunset"]
@@ -80,26 +77,20 @@ def search():
                 kmh=wind["speed_kph"],mph=wind["speed_mph"])
 
     elif (form.form["action"]=="forecast"):
-        # URLs for HTTP POST requests
-        url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
-
-        # Headers info { API key & and API host } to make API calls
-        headers = {
-            "X-RapidAPI-Key": "153f94ba4amsh2b77e486cbac9e4p18bfaejsn63e24918a3ba",
-            "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
-        }   
+        # URL for HTTP POST request for Forecast API
+        url = config.base_url+"forecast.json"
 
         # Take the entered user input 'city' to the query
         querystring = {"q":city,"days":"3"}
 
         # First API call for the real-time weather info in the city
-        forecast_response = requests.request("GET", url, headers=headers, params=querystring)
+        forecast_response = requests.request("GET", url, headers=config.headers, params=querystring)
         forecast_data = json.loads(forecast_response.text)
 
         # Exception Handling: invalid user input
         # When the user gives an unvalid input other than empty string
         # return 4xx Error 
-        if (forecast_response.status_code in range(400,500)):
+        if (forecast_response.status_code in range(400,501)):
             return render_template("index.html",error_message="No Matching Found Location")
         
         # Exception Handling: buggy codes/internal server issues
