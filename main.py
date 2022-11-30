397349bdcb1e651ecbf8f1919a53209ed9aa3d33
Daniel Ramirez <djram2898@outlook.com>
2022-11-29 23:16:29 -0600

    various changes to all files plus time zone page added

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def search():
         # When the user gives an unvalid input other than empty string
         # return 4xx Error 
         if (response_current.status_code in range(400,501)):
-            return render_template("index.html",error_message="No Matching Found Location")
+            return render_template("index.html",error_message="No Matching Location Found")
         
         # Exception Handling: buggy codes/internal server issues
         # When some codes are not working properly
@@ -91,7 +91,7 @@ def search():
         # When the user gives an unvalid input other than empty string
         # return 4xx Error 
         if (forecast_response.status_code in range(400,500)):
-            return render_template("index.html",error_message="No Matching Found Location")
+            return render_template("index.html",error_message="No Matching Location Found")
         
         # Exception Handling: buggy codes/internal server issues
         # When some codes are not working properly
@@ -100,6 +100,29 @@ def search():
             return render_template("error.html",status_code=forecast_response.status_code,error_message=forecast_response.reason)
 
         # Forecast Data is stored in the following variables
+
+        # Data for current day
+        today = str(forecast_data["forecast"]["forecastday"][0]["date"])
+        today_data = forecast_data["forecast"]["forecastday"][0]["day"]
+        today_astro = forecast_data["forecast"]["forecastday"][0]["astro"]
+
+        today_sunrise = today_astro["sunrise"]
+        today_sunset = today_astro["sunset"]
+
+        today_url = today_data["condition"]["icon"]
+
+        today_condition = today_data["condition"]["text"]
+        today_c_high = today_data["maxtemp_c"]
+        today_f_high = today_data["maxtemp_f"]
+        today_c_low = today_data["mintemp_c"]
+        today_f_low = today_data["mintemp_f"]
+        today_ws_kph = today_data["maxwind_kph"]
+        today_ws_mph = today_data["maxwind_mph"]
+        today_humidity = today_data["avghumidity"]
+        today_rain = today_data["daily_chance_of_rain"]
+        today_snow = today_data["daily_chance_of_snow"]
+
+        # Data for next day
         tomorrow = str(forecast_data["forecast"]["forecastday"][1]["date"])
         tomorrow_data = forecast_data["forecast"]["forecastday"][1]["day"]
         tomorrow_astro = forecast_data["forecast"]["forecastday"][1]["astro"]
@@ -120,6 +143,7 @@ def search():
         tomorrow_rain = tomorrow_data["daily_chance_of_rain"]
         tomorrow_snow = tomorrow_data["daily_chance_of_snow"]
 
+        # Data for day after next
         nday = str(forecast_data["forecast"]["forecastday"][2]["date"])
         nday_data = forecast_data["forecast"]["forecastday"][2]["day"]
         nday_astro = forecast_data["forecast"]["forecastday"][2]["astro"]
@@ -145,16 +169,66 @@ def search():
         else: 
             found_city = forecast_data["location"]["name"]+", "+forecast_data["location"]["region"]
         # Render variables to template 'forecast.html'
-        return render_template("forecast.html", location=found_city,tom_date=tomorrow, tom_url=tomorrow_url,
-                                tom_sunrise=tomorrow_sunrise, 
-                                tom_sunset=tomorrow_sunset, tom_desc=tomorrow_condition, 
+        return render_template("forecast.html", location=found_city,
+                                tod_date=today, tod_url=today_url,
+                                tod_sunrise=today_sunrise, tod_sunset=today_sunset, 
+                                tod_desc=today_condition, 
+                                tod_high_celsius=today_c_high, tod_high_fahrenheit=today_f_high,
+                                tod_low_celsius=today_c_low, tod_low_fahrenheit=today_f_low,
+                                tod_kmh=today_ws_kph, tod_mph=today_ws_mph, tod_hum=today_humidity,
+                                tod_rain_chance=today_rain, tod_snow_chance=today_snow,                                
+
+                                tom_date=tomorrow, tom_url=tomorrow_url,
+                                tom_sunrise=tomorrow_sunrise, tom_sunset=tomorrow_sunset, 
+                                tom_desc=tomorrow_condition, 
                                 tom_high_celsius=tomorrow_c_high, tom_high_fahrenheit=tomorrow_f_high,
                                 tom_low_celsius=tomorrow_c_low, tom_low_fahrenheit=tomorrow_f_low,
                                 tom_kmh=tomorrow_ws_kph, tom_mph=tomorrow_ws_mph, tom_hum=tomorrow_humidity,
-                                tom_rain_chance=tomorrow_rain, tom_snow_chance=tomorrow_snow, nd_date=nday,
-                                nd_url=nday_url,
+                                tom_rain_chance=tomorrow_rain, tom_snow_chance=tomorrow_snow, 
+
+                                nd_date=nday, nd_url=nday_url,
                                 nd_sunrise=nday_sunrise, nd_sunset=nday_sunset, nd_desc=nday_condition,
                                 nd_high_celsius=nday_c_high, nd_high_fahrenheit=nday_f_high, 
                                 nd_low_celsius=nday_c_low, nd_low_fahrenheit=nday_f_low, nd_kph=nday_ws_kph,
                                 nd_mph=nday_ws_mph, nd_hum=nday_humidity, nd_rain_chance=nday_rain,
                                 nd_snow_chance=nday_snow )
+
+
+    elif (form.form["action"]=="timezone"):
+        # URL for HTTP POST request for Forecast API
+        url = config.base_url+"timezone.json"
+
+        # Take the entered user input 'city' to the query
+        querystring = {"q":city}
+
+        # First API call for the real-time weather info in the city
+        timezone_response = requests.request("GET", url, headers=config.headers, params=querystring)
+        timezone_data = json.loads(timezone_response.text)
+
+        # Exception Handling: invalid user input
+        # When the user gives an unvalid input other than empty string
+        # return 4xx Error 
+        if (timezone_response.status_code in range(400,500)):
+            return render_template("index.html",error_message="No Matching Location Found")
+        
+        # Exception Handling: buggy codes/internal server issues
+        # When some codes are not working properly
+        # return 5xx Error
+        if (timezone_response.status_code in range(500,600)):
+            return render_template("error.html",status_code=forecast_response.status_code,error_message=forecast_response.reason)
+
+        # Time Zone Data is stored in the following variables
+        timezone_array = timezone_data["location"]
+        timezone_name = timezone_array["name"]
+        timezone_region = timezone_array["region"]
+        timezone_country = timezone_array["country"]
+        timezone_lat = timezone_array["lat"]
+        timezone_long = timezone_array["lon"]
+        timezone_id = timezone_array["tz_id"]
+        timezone_time = timezone_array["localtime"]
+
+        # Render variables to template 'forecast.html'
+        return render_template("timezone.html", location=timezone_name, tz_region=timezone_region,
+                                                tz_country=timezone_country, tz_lat=timezone_lat,
+                                                tz_long=timezone_long, tz_id=timezone_id, tz_time=timezone_time )
+
